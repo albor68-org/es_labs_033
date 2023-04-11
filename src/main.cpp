ccbc89417428d9d40c01ace3a3637c930bc08556
Ilya <gubanovilja2013@gmail.com>
2023-04-11 14:48:35 +0300

    Новый форк

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
-int main () {
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
 
-    while (true) {
+void gpio_setup();
+void gpio_use();
+
+int main () {
+    gpio_setup();
+
+while (true) {
+    gpio_use();
 
     }
+}
+
+void gpio_setup(){
+   rcc_periph_clock_enable(RCC_GPIOE);
+
+   gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9|GPIO11);
+}
+
+void gpio_use(){
+
+gpio_toggle(GPIOE, GPIO9|GPIO11);
 
 }
