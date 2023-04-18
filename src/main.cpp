0ad7f02f39fffd9c6c0e2bb4892d797a728cb8c9
denisdo <den.dorkin@mail.ru>
2023-04-18 13:00:51 +0300

    Лабораторная работа #4 УАПП и светодиод

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,46 @@
-#include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/usart.h>
 
-    void gpio_setup();
-    void gpio_use();
+void gpio_setup();
+#include <libopencm3/stm32/rcc.h>
+void gpio_use();
 
-    int main () {
-        gpio_setup ();
+int main () {
+    gpio_setup ();
 
-        while (true) {
-
-            gpio_use();
-        }
+    while (true) {
+        gpio_use();
+        usart_send_blocking(USART2,'H');
+        for(volatile uint32_t i = 0; i < 5'000'00;i++);
     }
+}
 
-    void gpio_setup(){
-        rcc_periph_clock_enable(RCC_GPIOE);
-        gpio_mode_setup(GPIOE,GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9|GPIO11|GPIO13|GPIO12);
-    }
-    void gpio_use (){
+void gpio_setup(){
 
-        gpio_toggle(GPIOE,GPIO9|GPIO11|GPIO13|GPIO15);
-    }
+    //Светодиод
+    rcc_periph_clock_enable(RCC_GPIOE);
+    gpio_mode_setup(GPIOE,GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO10|GPIO14);
+
+    //УАПП
+    rcc_periph_clock_enable(RCC_USART2);
+
+    usart_set_baudrate(USART2,115200); //скорость передачи
+    usart_set_databits (USART2,8);
+    usart_set_parity(USART2,USART_PARITY_NONE);
+    usart_set_stopbits(USART2,USART_STOPBITS_1); //Стоп-БИТ
+
+    usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+    usart_set_mode(USART2,USART_MODE_TX_RX);
+
+    rcc_periph_clock_enable(RCC_GPIOB);
+    gpio_mode_setup(GPIOB,GPIO_MODE_AF,GPIO_PUPD_NONE, GPIO3|GPIO4);
+    gpio_set_af(GPIOB,GPIO_AF7,GPIO3|GPIO4);
+
+    usart_enable(USART2);
+
+}
+void gpio_use (){
+    gpio_toggle(GPIOE,GPIO10|GPIO14);
+}
 
 
