c68dd46d77820aff0b5378b76c1ea8fbe5f303d7
LEX <13.pallex.13@gmail.com>
2023-04-25 15:14:00 +0300

    Лабораторная работа №5

--- src/main.cpp
+++ src/main.cpp
@@ -9,31 +9,47 @@ int main () {
     gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8|GPIO9|GPIO10|GPIO11|GPIO12|GPIO13|GPIO14|GPIO15);
 
     // УАПП
-    rcc_periph_clock_enable(RCC_USART2);
+    rcc_periph_clock_enable(RCC_USART3);
 
-    usart_set_baudrate(USART2, 115200);
-    usart_set_databits(USART2, 8);
-    usart_set_parity(USART2, USART_PARITY_NONE);
-    usart_set_stopbits(USART2, USART_STOPBITS_1);
+    usart_set_baudrate(USART3, 115200);
+    usart_set_databits(USART3, 8);
+    usart_set_parity(USART3, USART_PARITY_NONE);
+    usart_set_stopbits(USART3, USART_STOPBITS_1);
 
-    usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
-    usart_set_mode(USART2, USART_MODE_TX_RX);
+    usart_set_flow_control(USART3, USART_FLOWCONTROL_NONE);
+    usart_set_mode(USART3, USART_MODE_TX_RX);
 
+    // Контакты для УАПП
     rcc_periph_clock_enable(RCC_GPIOB);
-    gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO3|GPIO4);
-    gpio_set_af(GPIOB, GPIO_AF7, GPIO3|GPIO4);
-    usart_enable(USART2);
+    gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO10|GPIO11);
+    gpio_set_af(GPIOB, GPIO_AF7, GPIO10|GPIO11);
+    usart_enable(USART3);
 
-    usart_send_blocking(USART2, '>');
-    usart_send_blocking(USART2, '\r');
-    usart_send_blocking(USART2, '\n');
+    usart_send_blocking(USART3, '>');
+    usart_send_blocking(USART3, '\r');
+    usart_send_blocking(USART3, '\n');
 
     while (true) {
-        gpio_toggle(GPIOE, GPIO8|GPIO9|GPIO10|GPIO11|GPIO12|GPIO13|GPIO14|GPIO15);
-
-        uint16_t data = usart_recv_blocking(USART2);
-
-        usart_send_blocking(USART2, data);
+        uint16_t data = usart_recv_blocking(USART3);
+        switch(data) {
+            case 'N':
+                gpio_clear(GPIOE, GPIO8|GPIO9|GPIO10|GPIO11|GPIO12|GPIO13|GPIO14|GPIO15);
+                gpio_set(GPIOE, GPIO9|GPIO10);
+            break;
+            case 'E':
+                gpio_clear(GPIOE, GPIO8|GPIO9|GPIO10|GPIO11|GPIO12|GPIO13|GPIO14|GPIO15);
+                gpio_set(GPIOE, GPIO11|GPIO12);
+            break;
+            case 'S':
+                gpio_clear(GPIOE, GPIO8|GPIO9|GPIO10|GPIO11|GPIO12|GPIO13|GPIO14|GPIO15);
+                gpio_set(GPIOE, GPIO13|GPIO14);
+            break;
+            case 'W':
+                gpio_clear(GPIOE, GPIO8|GPIO9|GPIO10|GPIO11|GPIO12|GPIO13|GPIO14|GPIO15);
+                gpio_set(GPIOE, GPIO8|GPIO15);
+            break;
+        }
+    usart_send_blocking(USART3, data);
 
 
         //for(volatile uint32_t i = 0; i < 15'000; ++i);
