3b7ec8259e3ccf52e7f362bb940f07eb12a57edc
Polyanskaya_Sofa <sona080908@icloud.com>
2023-05-16 15:13:58 +0300

    APPARATNOE miganie

--- src/main.cpp
+++ src/main.cpp
@@ -14,21 +14,22 @@ int main () {
 
     rcc_periph_clock_enable(RCC_TIM6);
 
-   timer_set_prescaler(TIM6, rcc_get_timer_clk_freq(TIM6) / CK_CNT_Hz - 1 );
-   timer_set_period(TIM6, BLINK_PERIOD_MS - 1);
-   nvic_enable_irq(NVIC_TIM6_DAC_IRQ);
-   timer_enable_irq(TIM6, TIM_DIER_UIE);
+   timer_set_prescaler(TIM1, rcc_get_timer_clk_freq(TIM6) / CK_CNT_Hz - 1 );
+   timer_set_period(TIM1, BLINK_PERIOD_MS - 1);
 
-   timer_enable_counter(TIM6);
+   timer_set_oc_value(TIM1, TIM_OC4,BLINK_PERIOD_MS / 2 );
+   timer_set_oc_mode(TIM1, TIM_OC4, TIM_OCM_TOGGLE);
+   timer_enable_oc_output(TIM1, TIM_OC4);
+   timer_enable_break_main_output(TIM1);
+
+
+   timer_enable_counter(TIM1);
 
 
    rcc_periph_clock_enable(RCC_GPIOE);
-   gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9);
+   gpio_mode_setup(GPIOE, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO14);
+   gpio_set_af(GPIOE, GPIO_AF2, GPIO14 );
 
    while(true) {
    }
    }
-void tim6_dac_isr(void) {
-    timer_clear_flag(TIM6, TIM_SR_UIF);
-    gpio_toggle(GPIOE, GPIO9);
-}
