8cb913c625e71c01af18487df3cea9c7748be4b3
Ilya <gubanovilja2013@gmail.com>
2023-05-16 14:27:46 +0300

    Мигание светодиодом по прерыванию

--- src/main.cpp
+++ src/main.cpp
@@ -15,14 +15,17 @@ int main () {
     timer_set_period(TIM6, BLINK_PERIOD_MS - 1);
     timer_enable_counter(TIM6);
 
+    nvic_enable_irq(NVIC_TIM6_DAC_IRQ);
+    timer_enable_irq(TIM6, TIM_DIER_UIE);
 
     rcc_periph_clock_enable(RCC_GPIOE);
     gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9);
 
     while (true) {
-        if (timer_get_flag(TIM6, TIM_SR_UIF) !=0) {
-            gpio_toggle(GPIOE, GPIO9);
-            timer_clear_flag(TIM6, TIM_SR_UIF);
-        }
     }
+}
+
+void tim6_dac_isr(void) {
+    timer_clear_flag(TIM6, TIM_SR_UIF);
+    gpio_toggle(GPIOE, GPIO9);
 }
