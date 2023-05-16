392e3c687c61e8bbbdbfd7f62cb681cf51f06316
alekspo <Alexander>
2023-05-16 14:06:11 +0300

    miganie svetodiodom po oprosu

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,43 @@
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/timer.h>
+#include <libopencm3/cm3/nvic.h>
+
+constexpr uint16_t BLINK_PERIOD_MS{1000};
+constexpr uint16_t CK_CNT_Hz {1000};
+
+//constexpr float DUTY_CYCKE{0.4f};
+
+
+
 int main () {
 
-    while (true) {
-//
+    //  Настройка таймера
+    rcc_periph_clock_enable(RCC_TIM6);
+
+    timer_set_prescaler (TIM6, rcc_get_timer_clk_freq(TIM6) / CK_CNT_Hz - 1);
+    timer_set_period(TIM6,BLINK_PERIOD_MS - 1);
+
+    //
+    //
+
+    timer_enable_counter(TIM6);
+
+    //PORT
+
+    rcc_periph_clock_enable(RCC_GPIOE);
+
+    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9);
+
+    while(true) {
+        // Use timer
+        if (timer_get_flag(TIM6, TIM_SR_UIF) != 0){
+        gpio_toggle(GPIOE, GPIO9);
+        timer_clear_flag(TIM6, TIM_SR_UIF);
+        }
+        }
     }
 
-}
+
+
+
