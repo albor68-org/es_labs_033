1c83e8c5a9e2f3872d93dc0343e3ed3a6a8441c4
LEX <13.pallex.13@gmail.com>
2023-05-16 14:29:15 +0300

    Реализован прирыватель таймера

--- src/main.cpp
+++ src/main.cpp
@@ -19,15 +19,21 @@ int main () {
     timer_set_period(TIM6, BLINK_PERIOD_MS-1);
     timer_enable_counter(TIM6);
 
+    timer_enable_irq(TIM6, TIM_DIER_UIE);
+    nvic_enable_irq(NVIC_TIM6_DAC_IRQ);
+
+
     rcc_periph_clock_enable(RCC_GPIOE);
     gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9);
 
     while(true){
-        if (timer_get_flag(TIM6, TIM_SR_UIF)!=0){
-            gpio_toggle(GPIOE, GPIO9);
-            timer_clear_flag(TIM6, TIM_SR_UIF);
-        }
+       }
     }
+
+void tim6_dac_isr(void) {
+    timer_clear_flag(TIM6, TIM_SR_UIF);
+    gpio_toggle(GPIOE, GPIO9);
+}
 
 
 
@@ -82,4 +88,4 @@ int main () {
 //        //for(volatile uint32_t i = 0; i < 15'000; ++i);
 //    }
 
-}
+
