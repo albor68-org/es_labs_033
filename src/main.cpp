c9de63339be46fa0ab41366f6036e7a7ed9d2d08
alekspo <Alexander>
2023-05-16 15:12:56 +0300

    miganie apparatnoe

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,11 @@ int main () {
     timer_set_prescaler (TIM6, rcc_get_timer_clk_freq(TIM6) / CK_CNT_Hz - 1);
     timer_set_period(TIM6,BLINK_PERIOD_MS - 1);
 
+    timer_set_oc_value(TIM1,TIM_OC4, BLINK_PERIOD_MS /2);
+    timer_set_oc_mode(TIM1,TIM_OC4, TIM_OCM_TOGGLE);
+    timer_enable_oc_output(TIM1,TIM_OC4);
+    timer_enable_break_main_output(TIM1);
+
     nvic_enable_irq(NVIC_TIM6_DAC_IRQ);
     timer_enable_irq(TIM6, TIM_DIER_UIE);
 
@@ -31,6 +36,7 @@ int main () {
     rcc_periph_clock_enable(RCC_GPIOE);
 
     gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9);
+    gpio_set_af(GPIOE, GPIO_AF2, GPIO14);
 
     while(true) {
 
