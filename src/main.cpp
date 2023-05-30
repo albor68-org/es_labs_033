9925bbf728fcff2edaa70b81ef1fa84838426330
LEX <13.pallex.13@gmail.com>
2023-05-30 15:13:15 +0300

    ШИМ-сигнал с изменяемый скважностью

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,23 @@
 #include <libopencm3/stm32/timer.h>
 
 #include <libopencm3/cm3/nvic.h>
+#include <libopencm3/cm3/systick.h>
 
 
-constexpr uint16_t BLINK_PERIOD_MS{1000};
+constexpr uint16_t BLINK_PERIOD_MS{10};
 constexpr uint16_t CK_CNT_Hz{1000};
-constexpr float DOTY_CYCLE{0.4f};
+
+uint32_t ticks{0}; //Счётчик тиков системного таймера(время в мс)
+
+//constexpr float DOTY_CYCLE{0.4f};
 
 
 int main () {
+    //Настройка и запуск системного таймера
+    systick_set_frequency(CK_CNT_Hz, rcc_ahb_frequency);
+    systick_interrupt_enable();
+    systick_counter_enable();
+
 
     //Настройка таймера
     rcc_periph_clock_enable(RCC_TIM1);
@@ -18,8 +27,8 @@ int main () {
     timer_set_prescaler(TIM1, rcc_get_timer_clk_freq(TIM1)/CK_CNT_Hz-1);
     timer_set_period(TIM1, BLINK_PERIOD_MS-1);
 
-    timer_set_oc_value(TIM1, TIM_OC4, BLINK_PERIOD_MS/2);
-    timer_set_oc_mode(TIM1, TIM_OC4, TIM_OCM_TOGGLE);
+    timer_set_oc_value(TIM1, TIM_OC4, BLINK_PERIOD_MS/8);
+    timer_set_oc_mode(TIM1, TIM_OC4, TIM_OCM_PWM1);
     timer_enable_oc_output(TIM1, TIM_OC4);
     timer_enable_break_main_output(TIM1);
 
@@ -34,10 +43,19 @@ int main () {
     gpio_set_af(GPIOE, GPIO_AF2, GPIO14);
 
     while(true){
-       }
+        uint32_t ptime = ticks % 40000;
+
+        if  (ptime < 10000)timer_set_oc_value(TIM1, TIM_OC4, BLINK_PERIOD_MS/8);
+        else if  (ptime < 20000)timer_set_oc_value(TIM1, TIM_OC4, BLINK_PERIOD_MS/2);
+        else if  (ptime < 30000)timer_set_oc_value(TIM1, TIM_OC4, BLINK_PERIOD_MS/8*7);
+        else timer_set_oc_value(TIM1, TIM_OC4, BLINK_PERIOD_MS);
     }
+}
 
-
+//Обработка прирывания системного таймера
+void sys_tick_handler (void){
+    ticks++;
+}
 
 
 
